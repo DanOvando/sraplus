d522ff436eb3c3fc907205a06abb4da5e39beac9
DanOvando <dovando@bren.ucsb.edu>
2019-01-15 18:44:21 -0800

    try poaching travis config from rstan

---
+++ R/theme-sraplus.R
@@ -0,0 +1,12 @@
+#' plotting theme for sraplus
+#'
+#' @param base_size the font size
+#'
+#' @return a theme object
+#' @export
+#'
+theme_sraplus <- function(base_size = 12){
+  
+  out <- theme_light(base_size = base_size)
+  
+}
