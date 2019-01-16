673fef08dba0779a89e2a4dbcb9e5a9919211d0f
DanOvando <dovando@bren.ucsb.edu>
2019-01-15 17:46:00 -0800

    try adding thorsonutilies to remotes

--- R/plot-driors.R
+++ R/plot-driors.R
@@ -1,7 +1,33 @@
-plot_driors <- function(driors){
+plot_driors <- function(driors) {
+  timeseries <- dplyr::tibble(year = driors$years,
+                              catch = driors$catch)
   
-  timeseris <- dplyr::tibble(year = driors$years,
-                             catch = driors$catch)
+  if (any(!is.na(driors$index))) {
+    index_frame <- dplyr::tibble(year = driors$index_years,
+                                 index = driors$index)
+    timeseries <- timeseries %>%
+      dplyr::left_join(index_frame, by = "year")
+    
+  }
+  
+  if (any(!is.na(driors$u_v_umsy))) {
+    u_frame <- dplyr::tibble(year = driors$u_v_umsy,
+                             u_umsy = driors$u_years)
+    timeseries <- timeseries %>%
+      dplyr::left_join(u_frame, by = "year")
+    
+  }
+  
+  if (any(!is.na(driors$effort))) {
+    e_frame <- dplyr::tibble(year = driors$effort,
+                             effort = driors$effort_years)
+    timeseries <- timeseries %>%
+      dplyr::left_join(e_frame, by = "year")
+    
+  }
+  
+  
+  
   
   
 }
