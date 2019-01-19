7b47267074ecd69db8a314108069454860079e12
DanOvando <dovando@bren.ucsb.edu>
2019-01-18 17:18:24 -0800

    copy and paste TMB file rather than system build as a hack for PC cases that can't compile outside of working directory

--- R/get_tmb_model.R
+++ R/get_tmb_model.R
@@ -4,11 +4,19 @@
 #'
 #' @export
 #'
-get_tmb_model <- function(model_name = "sraplus_tmb" ){
-
-TMB::compile(system.file("tmb", paste0(model_name,".cpp"), package = "sraplus"))
-
-dyn.load(TMB::dynlib(
-  gsub(".cpp","",system.file("tmb", paste0(model_name,".cpp"), package = "sraplus"))))
-
+get_tmb_model <- function(model_name = "sraplus_tmb") {
+  if (!dir.exists(file.path(getwd(), "tmb"))) {
+    dir.create("tmb")
+    
+  }
+  
+  file.copy(from = system.file("tmb", paste0(model_name, ".cpp"), package = "sraplus"),
+            to =   file.path(getwd(), "tmb"),
+            overwrite = FALSE)
+  
+  TMB::compile(file.path("tmb", paste0(model_name, ".cpp")))
+  
+  dyn.load(TMB::dynlib(file.path("tmb", model_name)))
+  # gsub(".cpp","",system.file("tmb", paste0(model_name,".cpp"), package = "sraplus"))))
+  
 }
